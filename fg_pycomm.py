7a8cad641fb6010947ff2dd83787755346e8fdcb
alex.khoroshko <alex.khoroshko@gmail.com>
2015-03-21 12:16:05 +0700

    settings implementation started

--- fg_pycomm.py
+++ fg_pycomm.py
@@ -12,19 +12,39 @@
 #    You should have received a copy of the GNU General Public License
 #    along with fg-pycomm.  If not, see <http://www.gnu.org/licenses/>.
 
+def parse_settings():
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option("-s", "--settings", dest="settings_path",
+                  help="set settings file")
+    (options, args) = parser.parse_args()
+    return
+
+def parse_input_xml( ):
+    "this function parses xml with data settings"
+    from xml.dom import minidom
+    xmldoc = minidom.parse('../fg2comm.xml')
+    print (xmldoc.toxml())
+    return
 
 print('hello')
 
 if __name__ == '__main__':
     pass
 
+parse_settings();
+
 import socket
 UDP_IP = "127.0.0.1"
 UDP_PORT = 12345
+parse_input_xml();
 sock = socket.socket(socket.AF_INET, # Internet
                      socket.SOCK_DGRAM) # UDP
 sock.bind((UDP_IP, UDP_PORT))
 
+
+
 while True:
     data, addr = sock.recvfrom(1024) # buffer size is 1024 bytes
     print ("received message:"), data
+    
